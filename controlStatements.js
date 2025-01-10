541c9f1b043cc96d1de1dc9cf3f7d7940fc2d586
WabukoWabuko <basilwabbs@gmail.com>
2025-01-10 17:57:19 +0300

    Conditional Statement

---
+++ controlStatements.js
@@ -0,0 +1,24 @@
+// Conditional statement
+
+if (12 > 7) {
+    console.log("True")
+}
+
+// Else statement
+
+var age = 12
+if (age > 18) {
+    console.log("Adult")
+} else {
+    console.log("Kid")
+}
+
+// Else if Statement
+var marks = 101;
+if (marks > 100) {
+    console.log("Cannot be greater than 100")
+} else if (marks < 0){
+    console.log("Cannot be blow Zero")
+}else{
+    console.log("Reasonable Marks")
+}
