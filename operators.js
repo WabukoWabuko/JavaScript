02856fb5f2b0544934e69d02792d82715108e571
WabukoWabuko <basilwabbs@gmail.com>
2025-01-10 17:43:27 +0300

    Operators

---
+++ operators.js
@@ -0,0 +1,23 @@
+// Addition
+
+console.log(9 + 13)
+
+// Subtraction
+
+console.log(839 - 97)
+
+// Multiplication
+
+console.log(101 * 12)
+
+// Division
+
+console.log(172 / 45)
+
+// Modulus
+
+console.log(22 % 7)
+
+// Exponentiation
+
+console.log(7 ** 9)
--- variablesAndDataTypes.js
+++ variablesAndDataTypes.js
@@ -1,9 +1,22 @@
-var a = "Wabuko"
+var a = "Wabuko" // presumed to be the old way
 
-let b = 22;
+let b = 22; // Used for non-constant values
 
-const c = 60;
+const c = 60; // Used for constant values
 
 console.log(a)
 console.log(b)
 console.log(c)
+
+
+// Data Types
+let first = 17;
+let second = "WabukoWabuko";
+let third = "";
+let fourth = null;
+
+console.log("Type of First = " + (typeof first));
+console.log("Type of Second = " + (typeof second));
+console.log("Type of Third = " + (typeof third));
+console.log("Type of Fourth = " + (typeof fourth));
+console.log("Type of Fifth = " + (typeof e));
