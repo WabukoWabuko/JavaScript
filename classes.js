94c31f1440e07c224c23fb78c2b9257abfcdb2c2
WabukoWabuko <basilwabbs@gmail.com>
2025-01-12 00:14:12 +0300

    Classes

---
+++ classes.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    job(){
+        console.log("Computer Scientist");
+    }
+}
+
+let Person1 = new Person("Trenda", 55);
+let Person2 = new Person("Adams", 27);
+console.log(Person1)
+console.log(Person2)
